24bf301b2b17232c8512d6326737e81eb1bae72e
Руслана <ruslanakiverskaa@Ruslanas-iMac.local>
2009-07-03 23:46:03 +0800

    stylesheets

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,10 @@
 class PostsController < ApplicationController
+  def show
+     @post = Post.find(params[:id])
+   end
+
+   def index
+     @posts = Post.all(:limit => 10, :order => 'created_at DESC')
+     @posts = Post.paginate :page => params[:page], :per_page => 3
+   end
 end

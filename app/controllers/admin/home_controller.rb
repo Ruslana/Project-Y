60fb063fc0367aa7c085106bf921b307dea83e3e
Руслана <ruslanakiverskaa@u243.lan58.av>
2009-08-07 17:34:58 +0800

    deleted database.yml

---
+++ app/controllers/admin/home_controller.rb
@@ -0,0 +1,5 @@
+class Admin::HomeController < ProtectedController
+  def title
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   
   map.root :controller => "posts" 
+  map.login 'admin', :controller => "admin/home", :action => "title"
   
   map.namespace :admin do |admin|
     admin.resources :albums

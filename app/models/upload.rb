b3d2e153bc23912646bc2b508f9d80eb76581078
Руслана <ruslanakiverskaa@Ruslanas-iMac.local>
2009-10-09 18:53:47 +0800

    before_save

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,21 +1,16 @@
 class Upload < ActiveRecord::Base
   has_and_belongs_to_many :albums, :join_table => 'albums_uploads'
   
-  after_create :rename_music
-  after_update :rename_file
+  before_save :rename_file
   
   has_attached_file :music
   has_attached_file :file
   
   protected
   
-  def rename_music
-    update_attribute :name, music_file_name
-    update_attribute :title, file_file_name
-  end
-  
   def rename_file
-    update_attribute :title, file_file_name
+    self.title = file_file_name
+    self.name = music_file_name
   end
   
 end

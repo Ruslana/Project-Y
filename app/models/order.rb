dbf227f45fe508a5afe423a1668a04e437a06610
Руслана <ruslanakiverskaa@Ruslanas-iMac.local>
2009-11-03 10:49:55 +0700

    Authorizing payment

--- app/models/order.rb
+++ app/models/order.rb
@@ -30,5 +30,30 @@ class Order < ActiveRecord::Base
     transitions :from => :authorized, 
                 :to   => :authorized 
   end 
+  
+  def authorize_payment(credit_card, options = {}) 
+    options[:order_id] = number 
+    transaction do 
+      authorization = OrderTransaction.authorize(amount, credit_card, options) 
+      transactions.push(authorization) 
+      if authorization.success? 
+        payment_authorized! 
+      else 
+        transaction_declined! 
+      end 
+      authorization 
+    end 
+  end 
+  
+  def number 
+    "#{Time.now.to_i}-#{rand(1_000_000)}"
+  end 
+  
+  def authorization_reference 
+    if authorization = transactions.find_by_action_and_success('authorization', true, :order => 'id ASC') 
+      authorization.reference 
+    end 
+  end 
+  
     
 end

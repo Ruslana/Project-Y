7f4e746852e33386dc78e5cebc84481ed322cbc0
Ruslana <ruslana.kiverskaya@gmail.com>
2009-12-22 18:03:09 +0700

    deleted migrations

--- db/migrate/20091009063833_column_title.rb
+++
@@ -1,9 +0,0 @@
-class ColumnTitle < ActiveRecord::Migration
-  def self.up
-    add_column :uploads, :title, :string
-  end
-
-  def self.down
-    remove_column :uploads, :title
-  end
-end
--- db/migrate/20091009064620_rename_column.rb
+++
@@ -1,9 +0,0 @@
-class RenameColumn < ActiveRecord::Migration
-  def self.up
-    rename_column :uploads, :file_size_name, :file_file_size
-  end
-
-  def self.down
-    rename_column :uploads, :file_file_size, :file_size_name
-  end
-end

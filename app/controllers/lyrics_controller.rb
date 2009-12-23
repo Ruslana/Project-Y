b9ea6f39c5c1beffc10b3f80e3a2c30fc23c5539
Ruslana <ruslana.kiverskaya@gmail.com>
2009-12-23 13:46:04 +0700

    deleted controller lyrics & uploads_controller_test

--- app/controllers/lyrics_controller.rb
+++
@@ -1,9 +0,0 @@
-class LyricsController < ApplicationController
-  def show
-     @lyric = Lyric.find(params[:id])
-   end
-
-   def index
-     @lyrics = Lyric.paginate :page => params[:page], :per_page => 3
-   end
-end
